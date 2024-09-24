864e99c5e45de7843c9a74c200690e0dab1aba13
yggaraxyg <146045844+yggaraxyg@users.noreply.github.com>
2024-09-23 18:22:50 -0700

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FirstOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FirstOp.java
@@ -13,6 +13,7 @@ public class FirstOp extends LinearOpMode
     public DcMotor frontLeftWheel;
     public DcMotor backRightWheel;
     public DcMotor backLeftWheel;
+    public DcMotor leftArmMotor; //This is the arm on the left(if you are looking at the robot from behind)
     public double speedMultiplier =1;
 
     @Override
@@ -22,6 +23,7 @@ public class FirstOp extends LinearOpMode
         frontLeftWheel = hardwareMap.get(DcMotor.class,"FLMotor");
         backRightWheel = hardwareMap.get(DcMotor.class,"BRMotor");
         backLeftWheel = hardwareMap.get(DcMotor.class,"BLMotor");
+        leftArmMotor = hardwareMap.get(DcMotor.class,"LAMotor");
 
         telemetry.addData("Status","Intialized");
         telemetry.addData("Handicap", speedMultiplier);
@@ -31,12 +33,14 @@ public class FirstOp extends LinearOpMode
         frontLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
         double frontRightTargetPow=0;
         double frontLeftTargetPow=0;
         double backRightTargetPow=0;
         double backLeftTargetPow=0;
+        double leftArmTargetPow=0;
         while(opModeIsActive())
         {
             telemetry.addData("Status","Running");
@@ -66,7 +70,6 @@ public class FirstOp extends LinearOpMode
 
 
 
-
             if (gamepad1.right_trigger>=0.5){
                 if (speedMultiplier <1){
                     speedMultiplier = speedMultiplier +0.1;
@@ -74,7 +77,8 @@ public class FirstOp extends LinearOpMode
                 while (gamepad1.right_trigger>=0.5){
 
                 }
-            }else if (gamepad1.left_trigger>=0.5){
+            }
+            else if (gamepad1.left_trigger>=0.5){
                 if (speedMultiplier >0.1){
                     speedMultiplier = speedMultiplier -0.1;
                 }
@@ -83,6 +87,18 @@ public class FirstOp extends LinearOpMode
                 }
             }
             telemetry.addData("Handicap", speedMultiplier);
+
+            if (gamepad1.dpad_up){
+                leftArmTargetPow=1;
+            }
+            else if (gamepad1.dpad_down){
+                leftArmTargetPow=-1;
+            } else{
+                leftArmTargetPow=0;
+            }
+            telemetry.addData("Handicap", speedMultiplier);
+
+            leftArmMotor.setPower(leftArmTargetPow);
         }
     }
 
