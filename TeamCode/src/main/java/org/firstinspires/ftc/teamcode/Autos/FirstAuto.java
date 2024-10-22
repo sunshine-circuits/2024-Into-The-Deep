673ae473c1b4c470c974285d61a9e5e04fbcd0f3
Sunshine <yggaraxyg@gmail.com>
2024-10-21 18:41:43 -0700

    final submit for LMO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/FirstAuto.java
@@ -151,7 +151,7 @@ public class FirstAuto extends LinearOpMode{
         telemetry.update();
     }
     public void InchDrive(double distance, double angle, double pow, String telem){
-      gi   double pulses = InchesToPulses(distance);
+        double pulses = InchesToPulses(distance);
         int pulsesInt = (int)pulses;
         int FLBRMotion = (int)((Math.sin(angle) + Math.cos(angle)) * 537.7);
         int FRBLMotion = (int)((Math.sin(angle) - Math.cos(angle)) * 537.7);
