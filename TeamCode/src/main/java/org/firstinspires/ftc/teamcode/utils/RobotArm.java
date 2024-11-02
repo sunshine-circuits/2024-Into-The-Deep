3ed83bad50f348f682e9e9c60ec18e1be3ac48a5
Sunshine <yggaraxyg@gmail.com>
2024-11-01 18:12:09 -0700

    updating thigns from main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotArm.java
@@ -3,20 +3,31 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class RobotArm {
     private DcMotor armJoint;
     private DcMotor armExtend;
-
     private CRServo rightClaw;
     private CRServo leftClaw;
+    //This Double tracks the encoder position of the motor in the robot's linear actuator.
+    double armExtendPositionRelativeToBasisInPulses;
+    //this boolean tracks whether or not armExtend is in RUN_TO_POSITION mode.
+    boolean armExtendInRunToPosMode=false;
+    //this boolean tracks wheter or not armJoint's position needs set for the braking system.
+    boolean armJointPositionNeedsSet=true;
+    //this RunMode is the runmode motors are set to when they are first initalized.
+    private RunMode defaultMotorMode;
+    boolean zeroPositionFound=false;
+    double BasisInPulses;
 
     public enum Direction {
         DEPLOY,
         RETRACT,
         CLOCKWISE,
-        COUNTER_CLOCKWISE
+        COUNTER_CLOCKWISE,
+        BRAKE
     }
 
     public RobotArm(RobotConfig.Config config) {
@@ -24,6 +35,7 @@ public class RobotArm {
         this.armExtend = (DcMotor)config.get("ArmExtendMotor");
         this.rightClaw = (CRServo)config.get("RightServo");
         this.leftClaw = (CRServo)config.get("LeftServo");
+        defaultMotorMode=armExtend.getMode();
 
         this.rightClaw.setDirection(DcMotorSimple.Direction.REVERSE);
         this.leftClaw.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -31,6 +43,31 @@ public class RobotArm {
         armExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armJoint.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
+
+    //this double contains the number of pulses the encoder runs to move one rotation
+    private final double PULSES_PER_ROTATION = 537.7;
+
+    //this method accepts a double of inches and will return the number of pulses armExtend needs to rotate to move that far.
+//    public double InchesToPulses(double inches){
+//        return (inches/0.5)*PULSES_PER_ROTATION;
+//    }
+
+    //this method is a void that swaps armExtend to DefaultMotorMode if it is busy.
+//    public void runBackgroundArmExtendProcesses(){
+//        //swaps armExtend to DefaultMotorMode if it is busy
+//        if(armExtend.isBusy()){
+//            armExtendInRunToPosMode=true;
+//        }else{
+//            if(armExtendInRunToPosMode){
+//                armExtend.setMode(defaultMotorMode);
+//            }
+//        }
+//        if(zeroPositionFound==false){
+//            BasisInPulses=armExtend.getCurrentPosition();
+//        }
+//        armExtendPositionRelativeToBasisInPulses=armExtend.getCurrentPosition()-BasisInPulses;
+//    }
+
 
     //This method accepts a double of degrees and will rotate to that value as an absolute position.
     //0 degrees is straight up and down. Counter clockwise is negative and clockwise is positive.
@@ -46,8 +83,17 @@ public class RobotArm {
     //robot.
     public void rotateArmManual(Direction direction, double power) {
         switch (direction) {
-            case COUNTER_CLOCKWISE: armJoint.setDirection(DcMotorSimple.Direction.FORWARD); armJoint.setPower(power); break;
-            case CLOCKWISE: armJoint.setDirection(DcMotorSimple.Direction.REVERSE); armJoint.setPower(power); break;
+            case COUNTER_CLOCKWISE: armJoint.setMode(defaultMotorMode); armJoint.setDirection(DcMotorSimple.Direction.FORWARD); armJoint.setPower(power); armJointPositionNeedsSet=true; break;
+            case CLOCKWISE: armJoint.setMode(defaultMotorMode); armJoint.setDirection(DcMotorSimple.Direction.REVERSE); armJoint.setPower(power); armJointPositionNeedsSet=true; break;
+            case BRAKE:
+                armJoint.setDirection(DcMotorSimple.Direction.FORWARD);
+                armJoint.setPower(power);
+                if (armJointPositionNeedsSet) {
+
+                    armJoint.setTargetPosition(armJoint.getCurrentPosition());
+                    armJointPositionNeedsSet=false;
+                }
+                armJoint.setMode(DcMotor.RunMode.RUN_TO_POSITION); break;
         }
     }
 
@@ -55,8 +101,12 @@ public class RobotArm {
     //0 inches is fully retracted. X inches is fully extended. Ensure the arm will not attempt to
     //travel beyond the length of the arm nor retract past 0. This should also ensure we can not
     //extend the arm and collide with the robot.
-    public void extendArmTo(double inches) {
-
+    public void extendArmTo(double inches){
+        double pulsesFromZero=(inches/0.5)*PULSES_PER_ROTATION;
+        double pulsesNeeded=pulsesFromZero-armExtendPositionRelativeToBasisInPulses;
+        armExtendPositionRelativeToBasisInPulses=armExtendPositionRelativeToBasisInPulses+pulsesNeeded;
+        armExtend.setTargetPosition((int) (armExtend.getCurrentPosition()+pulsesNeeded));
+        armExtend.setMode(RunMode.RUN_TO_POSITION);
     }
 
     //This method accepts a double of power and direction and will extend/retract to a max/min value.
@@ -65,10 +115,24 @@ public class RobotArm {
     //extend the arm and collide with the robot.
     public void extendArmManual(Direction direction, double power) {
         switch (direction) {
-            case DEPLOY: armExtend.setDirection(DcMotorSimple.Direction.FORWARD); armExtend.setPower(power); break;
+            case DEPLOY:
+//            if(armExtendPositionRelativeToBasisInPulses>=InchesToPulses(7.5)){
+//
+//            }else{
+//                armExtend.setDirection(DcMotorSimple.Direction.FORWARD);
+//                armExtend.setPower(power);
+//            }
+//            break;
+                armExtend.setDirection(DcMotorSimple.Direction.FORWARD); armExtend.setPower(power); break;
             case RETRACT: armExtend.setDirection(DcMotorSimple.Direction.REVERSE); armExtend.setPower(power); break;
         }
     }
+    //this method, when called, reverses the motion of the arm
+//    public void resetExtender(){
+//        armExtend.setTargetPosition((int) (armExtend.getCurrentPosition()- InchesToPulses(8)));
+//        armExtend.setMode(RunMode.RUN_TO_POSITION);
+//        armExtendPositionRelativeToBasisInPulses=0;
+//    }
 
     //This method accepts a double of degrees and will rotate to that value as an absolute position.
     //0 degrees is straight up and down. Counter clockwise is negative and clockwise is positive.
