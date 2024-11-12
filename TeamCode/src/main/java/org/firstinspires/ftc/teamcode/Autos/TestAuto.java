118f94226711879754bafd9ee5f9d20bed3e3af9
ishaan-Github287 <ishaan.d.shah@outlook.com>
2024-11-11 17:21:27 -0800

    added a third autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/TestAuto.java
@@ -26,5 +26,13 @@ public class TestAuto extends InchAutoParent {
         telemetry.update();
         waitForStart();
         DistanceDrive(24,24,0.25,"Frick");
+
+        double autonomouspower = 0.25;//this is the power/speed in which the autonomous runs
+        double robotwidth = 18;
+        String AutonomousTelemetry = "Hello";//this is the specific telemetry which the drive functions(in the autonomous) use
+        DistanceDrive(24, 52, autonomouspower, AutonomousTelemetry);
+        DistanceDrive(30, 0, autonomouspower, AutonomousTelemetry);
+        DistanceDrive(6, 44 - robotwidth, autonomouspower, AutonomousTelemetry);
+        DistanceDrive(-48, 0, autonomouspower, AutonomousTelemetry);
     }
 }
